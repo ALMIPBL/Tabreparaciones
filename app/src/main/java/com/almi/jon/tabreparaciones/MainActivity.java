f720cffeb75f89a838f8a6334dc589adab7a53bd
kristianbilbo <33255579+kristianbilbo@users.noreply.github.com>
2017-11-02 09:44:13 +0100

    Initial commit

---
+++ app/src/main/java/com/almi/jon/tabreparaciones/MainActivity.java
@@ -0,0 +1,41 @@
+package com.almi.jon.tabreparaciones;
+
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private SectionsPageAdapter mSectionsPageAdapter;
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG,"onCreate: Starting.");
+
+         mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager){
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new TabConsolas(), "Consolas");
+        adapter.addFragment(new TabSmartphones(), "Smartphones y Tablets");
+        viewPager.setAdapter(adapter);
+
+    }
+
+}
